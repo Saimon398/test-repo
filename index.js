bb925c97817991f837235b3cbbccc1a8e0267b68
alex_p <aleksborislavskij@gmail.com>
2022-08-27 10:54:07 +0300

    style: formatted code style by eslint

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import _ from "lodash";
+import _ from 'lodash';
 // Импортировали библиотеку (пакет), поэтому не ставим .js в конце
-const result = _.last(["one", "two"]);
+const result = _.last(['one', 'two']);
 console.log(result);
